125f7aad35b69f8fc7e3395435d2c0653ac2e10f
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2023-03-01 21:45:55 +0530

    Question 2

---
+++ question2/insert_data.sql
@@ -0,0 +1,19 @@
+INSERT INTO Football_venue values(20001, 'Australia', 12345, 95000);
+
+INSERT INTO Football_venue values(20002, 'India', 77999, 96000);
+
+INSERT INTO Football_venue values(20003, 'USA', 33444, 81000);
+
+INSERT INTO Football_venue values(20004, 'England', 53525, 75000);
+
+INSERT INTO Football_venue values(20005, 'Australia', 52540, 63000);
+
+INSERT INTO Football_venue values(20006, 'Italy', 23234, 52000);
+
+INSERT INTO Football_venue values(200007, 'Egypt', 12522, 68000);
+
+INSERT INTO Football_venue values(20008, 'New York', 75757, 74000);
+
+INSERT INTO Football_venue values(20009, 'Dubai', 79797, 91000);
+
+INSERT INTO Football_venue values(20010, 'USA', 54564, 50000);
