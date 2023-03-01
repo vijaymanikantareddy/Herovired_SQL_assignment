2a076e57cc6f0c911cd7e692ad914a94eb184510
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2023-03-01 21:42:16 +0530

    Question 1

---
+++ question1/insert_details.sql
@@ -0,0 +1,19 @@
+INSERT INTO Employee_Details VALUES(68913, 'Mr. Singh', 'PRESIDENT', 68913, TO_DATE('1991-11-08','YYYY-MM-DD'), 6000, 1001);
+
+INSERT INTO Employee_Details VALUES(68232, 'Mr. Vijay', 'Chairman', 68232, TO_DATE('2002-05-25','YYYY-MM-DD'), 95000, 1000);
+
+INSERT INTO Employee_Details VALUES(63213, 'Mr. Neelesh', 'Full Stack Developer', 68324, TO_DATE('2002-07-20','YYYY-MM-DD'), 90000, 1003);
+
+INSERT INTO Employee_Details VALUES(66913, 'Mr. Suresh', 'HR', 68913, TO_DATE('2003-02-12','YYYY-MM-DD'), 85000, 1002);
+
+INSERT INTO Employee_Details VALUES(45613, 'Mr. Raj', 'Project Manager', 68913, TO_DATE('2001-10-18','YYYY-MM-DD'), 58000, 1001);
+
+INSERT INTO Employee_Details VALUES(68234, 'Mr. Jaswanth', 'Team Lead', 68913, TO_DATE('2002-04-05','YYYY-MM-DD'), 35000, 1002);
+
+INSERT INTO Employee_Details VALUES(62345, 'Mr. Siva', 'Data Analyst', 68913, TO_DATE('2002-11-28','YYYY-MM-DD'), 6000, 1003);
+
+INSERT INTO Employee_Details VALUES(43566, 'Mr. Ram', 'Project Manager', 68913, TO_DATE('2003-05-10','YYYY-MM-DD'), 70000, 1004);
+
+INSERT INTO Employee_Details VALUES(23455, 'Mr. Siddhu', 'Team Lead', 68913, TO_DATE('2003-03-28','YYYY-MM-DD'), 78000, 1002);
+
+INSERT INTO Employee_Details VALUES(56346, 'Mr. Sundar', 'Team Lead', 68913, TO_DATE('2001-01-08','YYYY-MM-DD'), 76000, 1000);
