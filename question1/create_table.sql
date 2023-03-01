2c40a363ac9846c561fb1ed5ceb60646d52a2d55
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2023-03-01 21:41:36 +0530

    Question 1

---
+++ question1/create_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Employee_Details
+    (
+    Emp_id number(5),
+    Emp_name varchar2(25),
+    Job_name varchar2(25),
+    Manager_id number(5),
+    Hire_date date,
+    Salary number(6),
+    dep_id number(4)
+   );
