8a569475731984ff2c7059bd77a8662f6e632f22
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2023-03-05 18:24:23 +0530

    Task 1

--- assignment2/task1.sql
+++ assignment2/task1.sql
@@ -50,7 +50,7 @@ WHERE DEPARTMENT = 'Admin';
 
 
 
-/* Q-8. Write an SQL query to print details of the Workers whose FIRST_NAME contains ‘a’ */
+/* Q-8. Write an SQL query to print details of the Workers whose FIRST_NAME contains ‘a’  */
 
 SELECT * FROM Worker 
 WHERE FIRST_NAME LIKE '%a%';
