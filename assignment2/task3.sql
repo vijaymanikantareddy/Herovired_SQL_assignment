18b3ad2bca1ceaaef120e07ba1bcdfb1995fe75b
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2023-03-06 06:25:47 +0530

    Task 3

--- assignment2/task3.sql
+++ assignment2/task3.sql
@@ -12,16 +12,18 @@ AND S.city = C.city;
 
 /* Q-14. Write a SQL query to find the salesperson(s) and the customer(s) he represents. Return Customer Name, city, Salesman, and commission. */
 
-SELECT C.cust_name, C.city, S.name, S.commission
-FROM Customer C, Salesman S
-WHERE C.salesman_id = C.salesman_id;
+SELECT C.cust_name AS "Customer Name", C.city, S.name AS "Salesman Name", S.commission
+FROM Customer C
+INNER JOIN Salesman S
+ON C.salesman_id = S.salesman_id;
 
 
 
 /* Q-15. Write a SQL query to find salespeople who received commissions of more than 12 percent from the company. 
 Return Customer Name, customer city, Salesman, and commission.  */
 
-SELECT C.cust_name, C.city, S.name, S.commission
-FROM Salesman S, Customer C
-WHERE S.salesman_id = C.salesman_id
-AND S.commission > 0.12;
+SELECT C.cust_name AS "Customer Name", C.city, S.name AS "Salesman Name", S.commission
+FROM Salesman S
+INNER JOIN Customer C
+ON S.salesman_id = C.salesman_id
+WHERE S.commission > 0.12;
